93afdbe515d146269537cb35a352bd44238cdd44
Isaías Víquez <82789832+IsaiasV22@users.noreply.github.com>
2026-09-10 17:56:15 -0600

    fix(api): update API URLs for books, authors, publishers, and reviews services

--- sites/books/src/api.js
+++ sites/books/src/api.js
@@ -1,5 +1,9 @@
-export const booksApi = "https://<tu-sitio-books-service>.netlify.app";
+export const booksApi =
+  "https://tarea3-sisdis-booksservice-isaiasv.netlify.app";
 
-export const authorsMinisite = "https://<tu-mini-sitio-de-autores>";
-export const publishersMinisite = "https://<tu-mini-sitio-de-editoriales>";
-export const reviewsMinisite = "https://<tu-mini-sitio-de-reviews>";
+export const authorsMinisite =
+  "https://tarea3-sisdis-authors-isaiasv.netlify.app";
+export const publishersMinisite =
+  "https://tarea3-sisdis-publishers-isaiasv.netlify.app";
+export const reviewsMinisite =
+  "https://tarea3-sisdis-reviews-isaiasv.netlify.app";
